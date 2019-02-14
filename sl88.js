806121505cdf3e32f11b52954737d055e6f5721e
Stchen Slem <stchen.sl@gmail.com>
2019-02-14 16:07:18 +0700

    Update sl88.js

--- sl88.js
+++ sl88.js
@@ -18,7 +18,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://blog.visitlinks.xyz/p/automobileinsurance.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);
+var x=Math.floor((Math.random()*5)+ 1);
+var xxx=null;
+if(x=="1"){xxx="virtual.html"}
+if(x=="2"){xxx="dedicated.html"}
+if(x=="3"){xxx="cheap.cloud.html"}
+if(x=="4"){xxx="big.data.html"}
+if(x=="5"){xxx="Tableau.html"}a_to_ve[i].href="http://data.visitlinks.xyz/2019/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
